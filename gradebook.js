e1ce1713ec6ed08092ee979d8f5d46e07a4c083d
Aleksandra Mate <matealeksandra@gmail.com>
2025-04-29 12:15:57 +0200

    Update gradebook.js

--- gradebook.js
+++ gradebook.js
@@ -7,3 +7,16 @@ function getAverage(scores) {
   
     return sum / scores.length;
   }
+
+function getGrade(score) {
+if (score === 100) { return 'A++'; }
+if (score >= 90 && score <= 99) { return 'A'; }
+if (score >= 80 && score <= 89) { return 'B'; }
+if (score >= 70 && score <= 79) { return 'C'; }
+if (score >= 60 && score <= 69) { return 'D'; }
+if (score <= 59) { return 'F'; }	
+}
+
+console.log(getGrade(96));
+console.log(getGrade(82));
+console.log(getGrade(56));
